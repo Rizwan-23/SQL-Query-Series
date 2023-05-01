08e9ca112259d7a1a00a98321decea04c599e5af
Rizwan-23 <125081051+Rizwan-23@users.noreply.github.com>
2023-05-01 23:15:08 +0530

    Update HackerRank-SQL(Intermediate)-Weather Observation Station 5.sql

--- HackerRank-SQL(Intermediate)-Weather Observation Station 5.sql
+++ HackerRank-SQL(Intermediate)-Weather Observation Station 5.sql
@@ -1,1 +1,19 @@
+------------------[𝐒𝐐𝐋 𝐈𝐧𝐭𝐞𝐫𝐦𝐞𝐝𝐢𝐚𝐭𝐞 𝐂𝐡𝐚𝐥𝐥𝐞𝐧𝐠𝐞 | 𝐖𝐞𝐚𝐭𝐡𝐞𝐫 𝐎𝐛𝐬𝐞𝐫𝐯𝐚𝐭𝐢𝐨𝐧 𝐒𝐭𝐚𝐭𝐢𝐨𝐧 𝟓 𝐅𝐫𝐨𝐦 𝐇𝐚𝐜𝐤𝐞𝐫𝐑𝐚𝐧𝐤]---------------------------
 
+SELECT city, LENGTH(city) AS length
+FROM Station
+WHERE LENGTH(city) = (
+    SELECT MIN(LENGTH(city))
+    FROM Station
+)
+ORDER BY city ASC
+LIMIT 1;
+
+SELECT city, LENGTH(city) AS length
+FROM Station
+WHERE LENGTH(city) = (
+    SELECT MAX(LENGTH(city))
+    FROM Station
+)
+ORDER BY city ASC
+LIMIT 1;
